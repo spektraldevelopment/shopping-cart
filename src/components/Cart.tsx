de76210bb7ab7f697ac774bbdf52a67e3c6cfeeb
Dave Boyle <spektraldevelopment@gmail.com>
2025-05-06 11:46:05 -0400

    [main] - Added grand total section.

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,4 +1,5 @@
 import "./cart.scss";
+import { CartItem } from "../types/types";
 import { useCart } from "../provider/useCart";
 import { resumeScroll } from "../utils/utils";
 
@@ -11,31 +12,66 @@ const Cart: React.FC = () => {
         resumeScroll();
     }
 
-    return(
+    const calculateTotalItem = (product: CartItem) => {
+
+        const { id, name, price, quantity } = product;
+
+        const itemTotal = quantity * price;
+
+        return (
+            <li key={id} className="cart-total__list--item">
+                <div>{name}({quantity})</div>
+                <div>${itemTotal}</div>
+            </li>
+        )
+    };
+
+    const calculateGrandTotal = () => {
+
+        let totalAmount = 0;
+
+        for (const product of cart) {
+            console.log("product: ", product)
+            const { price, quantity } = product;
+            totalAmount += quantity * price;
+        }
+
+        return totalAmount;
+    }
+
+    return (
         <div className={cartOpen ? "cart cart--active" : "cart"}>
             <div className="cart__header">
                 <h2>My Cart</h2>
                 <button onClick={onToggleCart}>Close</button>
             </div>
-            <ul className="cart-list">
-                {cart.map(product => (
-                    <li key={product.id} className="cart-list__item">
-                        <div className="cart-list__item--header">
-                            <h3>{product.name}</h3>
-                            <p>${product.price}</p>    
-                        </div>
-                        <img src={product.pic}/>
-                        <div>
-                            <p>Quantity: {product.quantity}</p>
-                            <div className="cart-list__buttons">
-                                <button onClick={() => addToCart(product)}>+</button>
-                                <button onClick={() => removeFromCart(product)}>-</button>
-                                <button onClick={() => deleteFromCart(product)}>Delete</button>
+            <div className="cart__content">
+                <ul className="cart-list">
+                    {cart.map(product => (
+                        <li key={product.id} className="cart-list__item">
+                            <div className="cart-list__item--header">
+                                <h3>{product.name}</h3>
+                                <p>${product.price}</p>
                             </div>
-                        </div>
-                    </li>
-                ))}
-            </ul>
+                            <img src={product.pic} />
+                            <div>
+                                <p>Quantity: {product.quantity}</p>
+                                <div className="cart-list__buttons">
+                                    <button onClick={() => addToCart(product)}>+</button>
+                                    <button onClick={() => removeFromCart(product)}>-</button>
+                                    <button onClick={() => deleteFromCart(product)}>Delete</button>
+                                </div>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+                <div className="cart-total">
+                    <ul className="cart-total__list">
+                        {cart.map(product => calculateTotalItem(product))}
+                    </ul>
+                    <div className="cart-total__amount">Grand Total: <span>${calculateGrandTotal()}</span></div>
+                </div>
+            </div>
         </div>
     );
 }
